43c5204982f1f9d8e30d168924fee0b7ce120445
1computer1 <onecomputer00@gmail.com>
2020-06-17 04:45:29 -0400

    lang(typescript): add DOM lib

--- languages/typescript/run.sh
+++ languages/typescript/run.sh
@@ -1,2 +1,2 @@
 cat > program.ts
-tsc --lib ESNext --target ES2019 --strict --skipLibCheck program.ts && cat program.js | node -p 
+tsc --lib DOM,ESNext --target ES2019 --strict --skipLibCheck program.ts && cat program.js | node -p 
